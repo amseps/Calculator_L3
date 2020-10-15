f0a00d83fbb23c3622642b414d8052f6a08b1a55
ffemale <59127653+ffemale@users.noreply.github.com>
2020-10-14 19:59:50 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Calculator_L3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator_L3.dir/main.cpp.obj"
+  "Calculator_L3.exe"
+  "Calculator_L3.exe.manifest"
+  "Calculator_L3.pdb"
+  "libCalculator_L3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calculator_L3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
